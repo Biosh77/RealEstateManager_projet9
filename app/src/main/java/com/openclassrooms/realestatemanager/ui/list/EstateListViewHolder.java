8ae44db4b623c4f642e6fea8e72ee4ee1e046d5b
Meunier Jordane <meunier.jordane@gmail.com>
2021-07-23 11:05:02 +0200

    - fixed ListviewHolder List picture instead of id

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/list/EstateListViewHolder.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/list/EstateListViewHolder.java
@@ -57,7 +57,7 @@ public class EstateListViewHolder extends RecyclerView.ViewHolder {
             itemSold.setImageResource(R.drawable.sold);
         }
         //Photo
-        if (!fullEstate.estate.getEstateID().isEmpty()) {
+        if (!fullEstate.myListPictures.isEmpty()) {
            glide.load(fullEstate.myListPictures.get(0).getPicturePath()).apply(RequestOptions.centerCropTransform()).into(itemImage);
         } else {
             glide.load(R.drawable.no_photo).apply(RequestOptions.centerCropTransform()).into(itemImage);
